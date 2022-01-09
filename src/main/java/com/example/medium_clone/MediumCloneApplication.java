414dcef4dd5060a6416d6190de98795cbea2ecb1
zzlev <gpalektzja@gmail.com>
2022-01-09 13:02:05 +0900

    Initial commit

---
+++ src/main/java/com/example/medium_clone/MediumCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.medium_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediumCloneApplication.class, args);
+	}
+
+}
