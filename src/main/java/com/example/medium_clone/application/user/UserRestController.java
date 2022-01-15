423ee1c15e016e07651105123343cc97927091de
zzlev <gpalektzja@gmail.com>
2022-01-15 13:24:13 +0900

    Add UserRestController.

---
+++ src/main/java/com/example/medium_clone/application/user/UserRestController.java
@@ -0,0 +1,13 @@
+package com.example.medium_clone.application.user;
+
+import com.example.medium_clone.application.user.dto.UserRegisterDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api/users")
+public class UserRestController {
+
+}
