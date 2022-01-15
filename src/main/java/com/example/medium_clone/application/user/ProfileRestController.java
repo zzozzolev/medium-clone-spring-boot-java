2e7f04b8279c8cdeb773721445233455ff20a082
zzlev <gpalektzja@gmail.com>
2022-01-15 13:33:38 +0900

    Add getProfile.

--- src/main/java/com/example/medium_clone/application/user/ProfileRestController.java
+++ src/main/java/com/example/medium_clone/application/user/ProfileRestController.java
@@ -1,5 +1,8 @@
 package com.example.medium_clone.application.user;
 
+import com.example.medium_clone.application.user.dto.ProfileGetDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/profiles")
 public class ProfileRestController {
 
+    @GetMapping("/{username}")
+    public ProfileGetDto getProfile(@PathVariable String username) {
+        ProfileGetDto dto = new ProfileGetDto();
+        dto.setUsername("demo");
+        dto.setBio("demo");
+        return dto;
+    }
 }
