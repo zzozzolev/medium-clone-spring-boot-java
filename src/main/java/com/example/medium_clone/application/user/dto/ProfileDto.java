2beb0a2b836fa212a769eee613d347f3b8158f49
zzlev <gpalektzja@gmail.com>
2022-01-15 13:40:12 +0900

    Rename ProfileGetDto to ProfileDto.

--- src/main/java/com/example/medium_clone/application/user/dto/ProfileDto.java
+++ src/main/java/com/example/medium_clone/application/user/dto/ProfileDto.java
@@ -3,7 +3,7 @@ package com.example.medium_clone.application.user.dto;
 import lombok.Data;
 
 @Data
-public class ProfileGetDto {
+public class ProfileDto {
 
     private String username;
     private String bio;
