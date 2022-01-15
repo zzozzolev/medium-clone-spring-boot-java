35bc2bbeb457b3ec716deed9b0733fb45d9566a8
zzlev <gpalektzja@gmail.com>
2022-01-15 13:40:52 +0900

    Add updateProfile.

--- src/main/java/com/example/medium_clone/application/user/ProfileRestController.java
+++ src/main/java/com/example/medium_clone/application/user/ProfileRestController.java
@@ -1,18 +1,23 @@
 package com.example.medium_clone.application.user;
 
-import com.example.medium_clone.application.user.dto.ProfileGetDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.medium_clone.application.user.dto.ProfileDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/profiles")
 public class ProfileRestController {
 
     @GetMapping("/{username}")
-    public ProfileGetDto getProfile(@PathVariable String username) {
-        ProfileGetDto dto = new ProfileGetDto();
+    public ProfileDto getProfile(@PathVariable String username) {
+        ProfileDto dto = new ProfileDto();
+        dto.setUsername("demo");
+        dto.setBio("demo");
+        return dto;
+    }
+
+    @PatchMapping("/{username}")
+    public ProfileDto updateProfile(@PathVariable String username) {
+        ProfileDto dto = new ProfileDto();
         dto.setUsername("demo");
         dto.setBio("demo");
         return dto;
