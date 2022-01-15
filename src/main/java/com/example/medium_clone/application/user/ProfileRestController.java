f904cbde02f902515f9cf7ca559c99dd52b0b479
zzlev <gpalektzja@gmail.com>
2022-01-15 13:26:20 +0900

    Add ProfileRestController.

---
+++ src/main/java/com/example/medium_clone/application/user/ProfileRestController.java
@@ -0,0 +1,10 @@
+package com.example.medium_clone.application.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profiles")
+public class ProfileRestController {
+
+}
