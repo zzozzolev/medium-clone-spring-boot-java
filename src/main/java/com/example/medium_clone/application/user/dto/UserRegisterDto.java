59c42b7ea6f79b089bc6defada7bd2c1eccbbd55
zzlev <30290423+zzozzolev@users.noreply.github.com>
2022-01-15 14:43:32 +0900

    feature: v1 Add User and Profile mock controller (#5)
    * Add UserRestController.
    
    * Add register post mapping.
    
    * Add UserRegisterDto.
    
    * Add ProfileRestController.
    
    * Add ProfileGetDto.
    
    * Add getProfile.
    
    * Rename ProfileGetDto to ProfileDto.
    
    * Add updateProfile.
    
    * Add h2.
    
    * Add swagger2.

---
+++ src/main/java/com/example/medium_clone/application/user/dto/UserRegisterDto.java
@@ -0,0 +1,11 @@
+package com.example.medium_clone.application.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegisterDto {
+
+    private String username;
+    private String password;
+    private String email;
+}
