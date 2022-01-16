04b0bb1e94d466f09e8188fdb2538a4f7005d4ab
zzlev <gpalektzja@gmail.com>
2022-01-16 16:51:38 +0900

    Use ProfileRepository findByUsername in getProfile.

--- src/main/java/com/example/medium_clone/application/user/ProfileRestController.java
+++ src/main/java/com/example/medium_clone/application/user/ProfileRestController.java
@@ -1,18 +1,23 @@
 package com.example.medium_clone.application.user;
 
 import com.example.medium_clone.application.user.dto.ProfileDto;
+import com.example.medium_clone.application.user.entity.Profile;
+import com.example.medium_clone.application.user.repository.ProfileRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.NoSuchElementException;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/profiles")
 public class ProfileRestController {
 
+    private final ProfileRepository profileRepository;
+
     @GetMapping("/{username}")
-    public ProfileDto getProfile(@PathVariable String username) {
-        ProfileDto dto = new ProfileDto();
-        dto.setUsername("demo");
-        dto.setBio("demo");
-        return dto;
+    public Profile getProfile(@PathVariable String username) {
+        return profileRepository.findByUsername(username).orElseThrow(NoSuchElementException::new);
     }
 
     @PatchMapping("/{username}")
