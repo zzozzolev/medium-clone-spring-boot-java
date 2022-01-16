e0dab9830b6afb85479b4c340f276ed6c2151ac3
zzlev <30290423+zzozzolev@users.noreply.github.com>
2022-01-16 16:56:43 +0900

    feature : Use find by username in getProfile (#13)
    * Add findByUsername to ProfileRepository.
    
    * Use ProfileRepository findByUsername in getProfile.

--- src/main/java/com/example/medium_clone/application/user/repository/ProfileRepository.java
+++ src/main/java/com/example/medium_clone/application/user/repository/ProfileRepository.java
@@ -3,6 +3,11 @@ package com.example.medium_clone.application.user.repository;
 
 import com.example.medium_clone.application.user.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+    Optional<Profile> findByUsername(@Param("username") String username);
 }
