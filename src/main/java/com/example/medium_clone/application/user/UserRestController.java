83bbc9ebbb157978ce204fbb3d93e176c6b14a3c
zzlev <gpalektzja@gmail.com>
2022-01-16 10:50:52 +0900

    Use UserService and UserRepository in UserRestController.

--- src/main/java/com/example/medium_clone/application/user/UserRestController.java
+++ src/main/java/com/example/medium_clone/application/user/UserRestController.java
@@ -1,17 +1,29 @@
 package com.example.medium_clone.application.user;
 
 import com.example.medium_clone.application.user.dto.UserRegisterDto;
+import com.example.medium_clone.application.user.entity.User;
+import com.example.medium_clone.application.user.repository.UserRepository;
+import com.example.medium_clone.application.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.NoResultException;
+import java.util.Optional;
+
 @RestController()
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserRestController {
 
+    private final UserService userService;
+    private final UserRepository userRepository;
+
     @PostMapping("/register")
-    public UserRegisterDto registerUser(@RequestBody UserRegisterDto dto) {
-        return dto;
+    public User registerUser(@RequestBody UserRegisterDto dto) {
+        Long memberId = userService.join(dto);
+        return userRepository.findById(memberId).orElseThrow(NoResultException::new);
     }
 }
