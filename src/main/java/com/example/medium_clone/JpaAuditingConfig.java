5f1775fbb4f189d0d3230cb86890f0677891ab1f
zzlev <30290423+zzozzolev@users.noreply.github.com>
2022-01-30 15:28:35 +0900

    Move @EnableJpaAuditing to JpaAuditingConfig class. (#23)

---
+++ src/main/java/com/example/medium_clone/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.example.medium_clone;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {}
