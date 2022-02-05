1ff1ea4e6a3700564d8003159a54abcd16c04579
zzlev <30290423+zzozzolev@users.noreply.github.com>
2022-02-05 20:37:46 +0900

    feature : Add Article entity (#27)
    * Add Article Entity.
    
    * Add RandomStringGenerator.
    
    * Add setSlugWithTitle to Article Entity.
    
    * Add testSetSlugWithTitleEqualLength to ArticleTest.
    
    * Add Slugify to build.gradle.
    
    * Add testSetSlugWithTitleGreaterLength to ArticleTest.
    
    * Delete exclusion for last hyphen of slug.
    
    * Add createArticle to Article and edit setSlugWithTitle.
    
    * Add articles to Profile.
    
    * Add setAuthor.
    
    * Edit getSlugWithTitle to setSlug.
    
    * Rename test to testRandomness.
    
    * Add org.apache.commons:commons-lang3:3.12.0 to build.gradle.
    
    * Remove RandomStringGenerator.
    
    * Use RandomStringUtils.randomAlphanumeric instead of RandomStringGenerator.
    
    * Add @Column to Article.
    
    * Add parameter validations and documentation to setAuthor and setSlug.
    
    * Make setAuthor,setSlug package private.
    
    * Add testSetAuthorNonNull.
    
    * Add setSlug validation tests.
    
    * Put static method createArticle under static member.
    
    * Set message to requireNonNull in setAuthor.
    
    * Remove @throws tag.

---
+++ src/main/java/com/example/medium_clone/application/article/entity/Article.java
@@ -0,0 +1,96 @@
+package com.example.medium_clone.application.article.entity;
+
+import com.example.medium_clone.application.common.entity.BaseTimeEntity;
+import com.example.medium_clone.application.user.entity.Profile;
+import com.github.slugify.Slugify;
+import lombok.Getter;
+import lombok.ToString;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@ToString(of = {"id", "slug"})
+public class Article extends BaseTimeEntity {
+
+    public static final int BODY_LENGTH = 255 * 4;
+
+    public static Article createArticle(Profile author, String title, String body, String description, Slugify slugify, int size, int maxSize) {
+        Article article = new Article();
+        article.setAuthor(author);
+        article.title = title;
+        article.setSlug(slugify, title, size, maxSize);
+        article.body = body;
+        article.description = description;
+
+        return article;
+    }
+
+    @Id @GeneratedValue
+    @Column(name = "article_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id")
+    private Profile author;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String slug;
+    @Column(nullable = false, length = BODY_LENGTH)
+    private String body;
+    private String description;
+
+    /**
+     * Set author to this article instance.
+     * Add this article to author's article list.
+     * @param author Profile instance.
+     */
+    void setAuthor(Profile author) {
+        Objects.requireNonNull(author, "author");
+        this.author = author;
+        author.getArticles().add(this);
+    }
+
+    /**
+     * Set slug to this article instance.
+     * Slug is generated using title and random alphanumeric.
+     * If the sum of slug and random string length is greater than 'maxSize',
+     * substring will be applied.
+     * @param slugify com.github.slugify instance.
+     * @param title article title.
+     * @param size Length of random string.
+     * @param maxSize Total length of slug and random string.
+     */
+    void setSlug(Slugify slugify, String title, int size, int maxSize) throws IllegalArgumentException {
+        // Validate parameters.
+        Objects.requireNonNull(slugify);
+        if (StringUtils.isBlank(title)) {
+            throw new IllegalArgumentException("title is blank.");
+        }
+        if (size <= 0) {
+            throw new IllegalArgumentException("size must be greater than zero.");
+        }
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("maxSize must be greater than zero.");
+        }
+        if (size > maxSize) {
+            throw new IllegalArgumentException("maxSize must be greater than or equal size.");
+        }
+
+        String slug = slugify.slugify(title);
+        String randomString = RandomStringUtils.randomAlphanumeric(size);
+
+        int randomHyphenSize = randomString.length() + 1;
+        // slug 길이와 랜덤 스트링 길이의 합이 최대 길이보다 큰 경우
+        if (slug.length() + randomHyphenSize > maxSize) {
+            slug = slug.substring(0, maxSize - randomHyphenSize);
+        }
+
+        this.slug = slug + "-" + randomString;
+    }
+}
---
+++ src/test/java/com/example/medium_clone/application/article/entity/ArticleTest.java
@@ -0,0 +1,118 @@
+package com.example.medium_clone.application.article.entity;
+
+import com.example.medium_clone.application.user.entity.Profile;
+import com.github.slugify.Slugify;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArticleTest {
+
+    private static final Slugify slugify = new Slugify();
+    private static final int size = 12;
+    private static final int maxSize = 300;
+
+    /*
+    slug.length() + randomHyphenSize == maxSize
+    substring 처리 안 하는 경우
+     */
+    @Test
+    public void testSetSlugWithTitleEqualLength() {
+        // given
+        // hyphen 길이가 더해져서 1을 추가로 빼줘야한다.
+        String title = "a".repeat(maxSize - size - 1);
+        Article article = new Article();
+
+        // when
+        article.setSlug(slugify, title, size, maxSize);
+
+        // then
+        assertThat(article.getSlug().length()).isEqualTo(maxSize);
+    }
+
+    /*
+    slug.length() + randomHyphenSize == maxSize + 1
+    substring 처리하는 경우
+     */
+    @Test
+    public void testSetSlugWithTitleGreaterLength() {
+        // given
+        // hyphen 길이가 더해져서 1을 추가로 빼주지 않아도 된다.
+        String title = "a".repeat(maxSize - size);
+        Article article = new Article();
+
+        // when
+        article.setSlug(slugify, title, size, maxSize);
+
+        // then
+        assertThat(article.getSlug().length()).isEqualTo(maxSize);
+    }
+
+    @Test
+    public void testSetSlugSlugifyNonNull() {
+        // given
+        Article article = new Article();
+        Slugify slugify = null;
+        String title = "title";
+        int size = 10;
+        int maxSize = 20;
+
+        Assertions.assertThrows(NullPointerException.class, () -> article.setSlug(slugify, title, size, maxSize));
+    }
+
+    @Test
+    public void testSetSlugTitleNotBlank() {
+        // given
+        Article article = new Article();
+        String title = " ";
+        int size = 10;
+        int maxSize = 20;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> article.setSlug(slugify, title, size, maxSize));
+    }
+
+    @Test
+    public void testSetSlugSizeGtZero() {
+        // given
+        Article article = new Article();
+        String title = " ";
+        int size = 0;
+        int maxSize = 20;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> article.setSlug(slugify, title, size, maxSize));
+    }
+
+    @Test
+    public void testSetSlugMaxSizeGtZero() {
+        // given
+        Article article = new Article();
+        String title = " ";
+        int size = 10;
+        int maxSize = 0;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> article.setSlug(slugify, title, size, maxSize));
+    }
+
+    @Test
+    public void testSetSlugMaxSizeGteSize() {
+        // given
+        Article article = new Article();
+        String title = " ";
+        int size = 10;
+        int maxSize = 9;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> article.setSlug(slugify, title, size, maxSize));
+    }
+
+    @Test
+    public void testSetAuthorNonNull() {
+        // given
+        Profile profile = null;
+        Article article = new Article();
+
+        // when, then
+        Assertions.assertThrows(NullPointerException.class, () -> article.setAuthor(profile));
+    }
+
+}
