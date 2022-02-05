013f7ca781d81f4fc174db67c6158b1942385adc
zzlev <gpalektzja@gmail.com>
2022-02-05 12:52:58 +0900

    Add parameter validations and documentation to setAuthor and setSlug.

--- src/main/java/com/example/medium_clone/application/article/entity/Article.java
+++ src/main/java/com/example/medium_clone/application/article/entity/Article.java
@@ -6,8 +6,10 @@ import com.github.slugify.Slugify;
 import lombok.Getter;
 import lombok.ToString;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -43,12 +45,44 @@ public class Article extends BaseTimeEntity {
         return article;
     }
 
+    /**
+     * Set author to this article instance.
+     * Add this article to author's article list.
+     * @param author Profile instance.
+     */
     public void setAuthor(Profile author) {
+        Objects.requireNonNull(author);
         this.author = author;
         author.getArticles().add(this);
     }
 
-    public void setSlug(Slugify slugify, String title, int size, int maxSize) {
+    /**
+     * Set slug to this article instance.
+     * Slug is generated using title and random alphanumeric.
+     * If the sum of slug and random string length is greater than 'maxSize',
+     * substring will be applied.
+     * @param slugify com.github.slugify instance.
+     * @param title article title.
+     * @param size Length of random string.
+     * @param maxSize Total length of slug and random string.
+     * @throws IllegalArgumentException
+     */
+    public void setSlug(Slugify slugify, String title, int size, int maxSize) throws IllegalArgumentException {
+        // Validate parameters.
+        Objects.requireNonNull(slugify);
+        if (StringUtils.isBlank(title)) {
+            throw new IllegalArgumentException("title is blank.");
+        }
+        if (size <= 0) {
+            throw new IllegalArgumentException("size must be greater than zero.");
+        }
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("maxSize must be greater than zero.");
+        }
+        if (size > maxSize) {
+            throw new IllegalArgumentException("maxSize must be greater than or equal size.");
+        }
+
         String slug = slugify.slugify(title);
         String randomString = RandomStringUtils.randomAlphanumeric(size);
 
