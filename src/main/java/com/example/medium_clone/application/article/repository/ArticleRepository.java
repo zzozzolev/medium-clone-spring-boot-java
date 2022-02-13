b56938fea657d1e426396f022476ddac716d315e
zzlev <gpalektzja@gmail.com>
2022-02-13 13:22:42 +0900

    Add findBySlugFetchAuthor to ArticleRepository.

--- src/main/java/com/example/medium_clone/application/article/repository/ArticleRepository.java
+++ src/main/java/com/example/medium_clone/application/article/repository/ArticleRepository.java
@@ -2,12 +2,14 @@ package com.example.medium_clone.application.article.repository;
 
 import com.example.medium_clone.application.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
-    Optional<Article> findBySlug(@Param("slug") String slug);
+    @Query("select a from Article a left join fetch a.author where a.slug = :slug")
+    Optional<Article> findBySlugFetchAuthor(@Param("slug") String slug);
 
 }
