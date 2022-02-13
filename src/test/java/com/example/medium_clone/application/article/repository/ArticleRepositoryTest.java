4f3356fbfe1832f1581dfee095ef204c5d30c535
zzlev <gpalektzja@gmail.com>
2022-02-13 13:31:55 +0900

    Add testFindBySlugFetchAuthor to ArticleRepositoryTest.

--- src/test/java/com/example/medium_clone/application/article/repository/ArticleRepositoryTest.java
+++ src/test/java/com/example/medium_clone/application/article/repository/ArticleRepositoryTest.java
@@ -4,10 +4,16 @@ import com.example.medium_clone.application.article.entity.Article;
 import com.example.medium_clone.application.user.entity.Profile;
 import com.example.medium_clone.application.user.repository.ProfileRepository;
 import com.github.slugify.Slugify;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -25,4 +31,22 @@ class ArticleRepositoryTest {
         Article article = Article.createArticle(profile, "test", "Test", "test", new Slugify(), 20, 100);
         articleRepository.save(article);
     }
+
+    @Test
+    public void testFindBySlugFetchAuthor() {
+        // given
+        Profile profile = Profile.createProfile("test", "test");
+        profileRepository.save(profile);
+
+        Article article = Article.createArticle(profile, "test", "Test", "test", new Slugify(), 20, 100);
+        articleRepository.save(article);
+
+        // when
+        Article foundArticle = articleRepository.findBySlugFetchAuthor(article.getSlug()).orElseThrow(NoSuchElementException::new);
+
+        // then
+        assertThat(foundArticle).isEqualTo(article);
+        assertThat(foundArticle.getAuthor()).isEqualTo(profile);
+    }
+
 }
