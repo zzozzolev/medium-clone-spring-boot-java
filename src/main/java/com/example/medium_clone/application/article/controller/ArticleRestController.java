2b318b6e1e2e0b8c4c1dea0fa48fb95135e986bc
zzlev <gpalektzja@gmail.com>
2022-02-13 12:33:21 +0900

    Remove unused packages in ArticleRestController.

--- src/main/java/com/example/medium_clone/application/article/controller/ArticleRestController.java
+++ src/main/java/com/example/medium_clone/application/article/controller/ArticleRestController.java
@@ -4,8 +4,6 @@ import com.example.medium_clone.application.article.dto.ArticleCreateDto;
 import com.example.medium_clone.application.article.entity.Article;
 import com.example.medium_clone.application.article.repository.ArticleRepository;
 import com.example.medium_clone.application.article.service.ArticleService;
-import com.example.medium_clone.application.user.entity.Profile;
-import com.github.slugify.Slugify;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
