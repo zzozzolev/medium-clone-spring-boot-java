7eacd588886365c77fba1636d9a61d8de8a9a9fb
zzlev <gpalektzja@gmail.com>
2022-02-13 12:51:04 +0900

    Add testGetArticle to ArticleRestControllerTest.

--- src/test/java/com/example/medium_clone/application/article/controller/ArticleRestControllerTest.java
+++ src/test/java/com/example/medium_clone/application/article/controller/ArticleRestControllerTest.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ArticleRestController.class)
@@ -70,6 +71,22 @@ class ArticleRestControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testGetArticle() throws Exception {
+        // given
+        ArticleCreateDto dto = getArticleCreateDto();
+        Profile author = Profile.createProfile("bio", dto.getUsername());
+        Article article = Article.createArticle(author, dto.getTitle(), dto.getBody(), dto.getDescription(), slugify, 10, 200);
+        String slug = article.getSlug();
+
+        // mocking
+        when(articleRepository.findBySlug(slug)).thenReturn(Optional.of(article));
+
+        // then
+        mockMvc.perform(get(commonPath + "/" + slug))
+                .andExpect(status().isOk());
     }
 
     private ArticleCreateDto getArticleCreateDto() {
