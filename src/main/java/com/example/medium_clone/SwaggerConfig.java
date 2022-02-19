dc46caca2c75d8dc1ae49f442a3ed997c6e845bb
zzlev <30290423+zzozzolev@users.noreply.github.com>
2022-02-19 14:27:29 +0900

    hotfix: Show sort param in swagger (#40)
    * Add Page class and use it in swagger.
    
    * Set @SortDefault in getArticles.

--- src/main/java/com/example/medium_clone/SwaggerConfig.java
+++ src/main/java/com/example/medium_clone/SwaggerConfig.java
@@ -1,9 +1,16 @@
 package com.example.medium_clone;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,10 +21,28 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        TypeResolver typeResolver = new TypeResolver();
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class))
+                )
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Getter @Setter
+    @ApiModel
+    static class Page {
+        @ApiModelProperty(value = "page")
+        private Integer page;
+
+        @ApiModelProperty(value = "page size", allowableValues = "range[0, 10]")
+        private Integer size;
+
+        @ApiModelProperty(value = "sort[ex) created_date,(asc|desc)]")
+        private String sort;
     }
 }
