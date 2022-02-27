57719b38d5196dd0c29dc5f9555e77a2da5ef37d
zzlev <30290423+zzozzolev@users.noreply.github.com>
2022-02-27 18:13:20 +0900

    feature: Add simple login logout (#48)
    * Add spring-boot-starter-security.
    
    * Add login form.
    
    * Implement UserDetails in User.
    
    * Add findByEmail to UserRepository.
    
    * Add UserDetailServiceImpl.
    
    * Add PlainPasswordEncoder.
    
    * Make PlainPasswordEncoder bean in SecurityConfig.
    
    * Set username parameter as email.
    
    * Add authentication to any requests except for login and register.
    
    * Fix wrong package import.
    
    * Add @WithMockUser annotation to article and profile controller test.

---
+++ src/main/java/com/example/medium_clone/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.medium_clone;
+
+import com.example.medium_clone.web.auth.PlainPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/api/users/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .usernameParameter("email");
+    }
+
+    @Bean
+    public PlainPasswordEncoder passwordEncoder() {
+        return new PlainPasswordEncoder();
+    }
+}
--- src/main/java/com/example/medium_clone/application/user/repository/UserRepository.java
+++ src/main/java/com/example/medium_clone/application/user/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.example.medium_clone.application.user.repository;
 
 import com.example.medium_clone.application.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(@Param("email") String email);
 
 }
