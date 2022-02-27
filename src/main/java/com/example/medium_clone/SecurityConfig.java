20a11d07916cc0085f0b6daf810dfdb793114fc1
zzlev <gpalektzja@gmail.com>
2022-02-27 20:42:29 +0900

    Use BCryptPasswordEncoder in SecurityConfig.

--- src/main/java/com/example/medium_clone/SecurityConfig.java
+++ src/main/java/com/example/medium_clone/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PlainPasswordEncoder passwordEncoder() {
-        return new PlainPasswordEncoder();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
