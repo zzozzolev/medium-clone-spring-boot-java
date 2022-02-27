d0bc46d949cb13dc0845e19ee93c3e562664f2d4
zzlev <gpalektzja@gmail.com>
2022-02-27 17:35:02 +0900

    Make PlainPasswordEncoder bean in SecurityConfig.

--- src/main/java/com/example/medium_clone/SecurityConfig.java
+++ src/main/java/com/example/medium_clone/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.medium_clone;
 
+import com.example.medium_clone.web.auth.PlainPasswordEncoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,5 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .formLogin();
+    }
+
+    @Bean
+    public PlainPasswordEncoder passwordEncoder() {
+        return new PlainPasswordEncoder();
     }
 }
