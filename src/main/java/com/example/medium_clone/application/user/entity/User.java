18aeba7562cacf13b43146f5f5746daede8fa493
zzlev <gpalektzja@gmail.com>
2022-02-27 17:33:08 +0900

    Implement UserDetails in User.

--- src/main/java/com/example/medium_clone/application/user/entity/User.java
+++ src/main/java/com/example/medium_clone/application/user/entity/User.java
@@ -4,13 +4,16 @@ import com.example.medium_clone.application.common.entity.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Getter
 @ToString(of = {"id", "email"})
-public class User extends BaseTimeEntity {
+public class User extends BaseTimeEntity implements UserDetails {
 
     @Id @GeneratedValue
     @Column(name = "user_id")
@@ -33,4 +36,38 @@ public class User extends BaseTimeEntity {
         this.profile = profile;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
