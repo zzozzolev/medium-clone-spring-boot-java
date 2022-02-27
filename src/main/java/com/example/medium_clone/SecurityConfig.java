ad700e97e183eb8f3e6caf60ff3fff69b1e5a7b9
zzlev <gpalektzja@gmail.com>
2022-02-27 20:45:54 +0900

    Remove PlainPasswordEncoder.

--- src/main/java/com/example/medium_clone/SecurityConfig.java
+++ src/main/java/com/example/medium_clone/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.medium_clone;
 
-import com.example.medium_clone.web.auth.PlainPasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
