1c4a994ae2844c95a54032052162c3821f60b084
zzlev <gpalektzja@gmail.com>
2022-03-01 16:06:46 +0900

    Use CustomUsernamePasswordAuthenticationFilter.

--- src/main/java/com/example/medium_clone/SecurityConfig.java
+++ src/main/java/com/example/medium_clone/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.medium_clone;
 
+import com.example.medium_clone.web.auth.filter.CustomUsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +16,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable().authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .csrf().disable()
+                .addFilterAt(getAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/api/login").permitAll()
                 .antMatchers("/api/users/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                    .usernameParameter("email");
+                    .formLogin().disable()
+                .logout().logoutUrl("/api/logout");
+    }
+
+    protected CustomUsernamePasswordAuthenticationFilter getAuthFilter() throws Exception{
+        CustomUsernamePasswordAuthenticationFilter filter = new CustomUsernamePasswordAuthenticationFilter();
+        filter.setFilterProcessesUrl("/api/login");
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setUsernameParameter("email");
+        return filter;
     }
 
     @Bean
